cf17b2d3f3f92117067811974b604eae8d276911
horsecock <73473096+sirhorse-cawk@users.noreply.github.com>
2020-10-28 10:47:37 +0100

    commit

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 
 <html>
 <head>
+    <link rel="css" href="index.css">
     <meta charset="UTF-8">
     <title>Monkey Business</title>
 </head>
 <body>
+
 <img src="img/monkey-business.jpg" alt="monkey-business">
 
 <h3> Select your monkey! </h3>
