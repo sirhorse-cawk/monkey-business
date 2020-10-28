e016433f25ded5bd6f410cbe013ebe1278b66e09
horsecock <73473096+sirhorse-cawk@users.noreply.github.com>
2020-10-28 11:14:06 +0100

    commit

--- index.php
+++ index.php
@@ -1,19 +1,51 @@
 <?php
+
+$apen[] = "Baviaan";
+$apen[] = "Guereza";
+$apen[] = "Langoer";
+$apen[] = "Tamarin";
+$apen[] = "Neusaap";
+$apen[] = "Halfaap";
+$apen[] = "Mandril";
+$apen[] = "Oeakari";
+$apen[] = "Faunaap";
+$apen[] = "Hoelman";
+$apen[] = "Meerkat";
+$apen[] = "Oormaki";
+$apen[] = "Gorilla";
+$apen[] = "Kuifaap";
+$apen[] = "Mensaap";
+$apen[] = "Spinaap";
+
 ?>
 
 <html>
+
+
+
 <head>
     <link rel="stylesheet" href="stylesheet.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <title>Monkey Business</title>
 </head>
+
+
+
 <body>
 
 <img src="img/monkey-business.jpg" alt="monkey-business">
 
-<h3> Select your monkey! </h3>
+<h2> Select your monkey! </h2>
 
 <img src="img/monkey_swings.png" alt="monkey_swings">
 
+<?php foreach($apen as $aap) { ?>
+    <p <?php echo $aap; ?> </p>
+<?php } ?>
+
 </body>
+
+
+
 </html>
