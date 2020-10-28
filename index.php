93b3c225df8f22f3b5893994bff82ae67d8e341c
horsecock <73473096+sirhorse-cawk@users.noreply.github.com>
2020-10-28 10:11:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Monkey Business</title>
+</head>
+<body>
+<img src="" alt="">
+
+<h3> Select your monkey! </h3>
+
+<img src="" alt="">
+
+</body>
+</html>
